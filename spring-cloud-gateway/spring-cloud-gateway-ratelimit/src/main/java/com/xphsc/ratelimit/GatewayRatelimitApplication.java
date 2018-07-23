b5f90f44224f8417f2df05dc43004061014fd34b
huipei.x <xiongpeih@163.com>
2018-07-23 22:23:58 +0800

    no message

---
+++ spring-cloud-gateway/spring-cloud-gateway-ratelimit/src/main/java/com/xphsc/ratelimit/GatewayRatelimitApplication.java
@@ -0,0 +1,21 @@
+package com.xphsc.ratelimit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.cloud.netflix.zuul.EnableZuulServer;
+
+/**
+ * Created by ${huipei.x} on 2018/4/24.
+ */
+@EnableZuulServer
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+public class GatewayRatelimitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayRatelimitApplication.class, args);
+    }
+}
